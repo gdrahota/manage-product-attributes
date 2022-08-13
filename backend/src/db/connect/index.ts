ca0b85154f454795d2a79f54841ae3fdcd09d82f
Guido Drahota <guido.drahota@kaulsdorf-it-solutions.de>
2022-08-13 22:20:58 +0200

    deployment changed (1)

--- backend/src/db/connect/index.ts
+++ backend/src/db/connect/index.ts
@@ -7,13 +7,14 @@ const { POSTGRES_PORT, POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_USER, TABLE_CA
 
 const postgresConnectionConfig = {
   client: 'pg',
-  connection: {
-    host: POSTGRES_HOST,
-    port: POSTGRES_PORT,
-    user: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-    database: TABLE_CATALOG,
-  },
+  connection: 'postgresql://db:AVNS_9f1qrzBt7L2yD1nchP2@app-5b5ca2ae-44fb-47c4-88a7-4faa3d4a29e8-do-user-12230756-0.b.db.ondigitalocean.com:25060/db?sslmode=require',
+  //   {
+  //   host: POSTGRES_HOST,
+  //   port: POSTGRES_PORT,
+  //   user: POSTGRES_USER,
+  //   password: POSTGRES_PASSWORD,
+  //   database: TABLE_CATALOG,
+  // },
   debug: false,
   searchPath: [ SCHEMA_NAME ],
 }
