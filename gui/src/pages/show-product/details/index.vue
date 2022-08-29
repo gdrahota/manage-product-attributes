048c3e0d74c55a2d99d5f762cb367a6920fa2fad
Guido Drahota <guido.drahota@kaulsdorf-it-solutions.de>
2022-08-29 13:39:37 +0200

    GUI: product page refactored

---
+++ gui/src/pages/show-product/details/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-card flat>
+    <q-card-section>
+      <div class="row">
+        <div class="col">
+          <attributes
+              v-for="productGroup of productGroups"
+              :key="productGroup.id"
+              :product="product"
+              :product-group="productGroup"
+              class="q-mb-md"
+          />
+        </div>
+
+        <div class="col">
+          <files :files="product.files" />
+        </div>
+      </div>
+
+      <q-field
+          label="Description"
+          outlined
+          stack-label
+      >
+        <template v-slot:control>
+          <div v-html="product.description" />
+        </template>
+      </q-field>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import Attributes from './attributes'
+import Files from './files'
+
+export default {
+  components: {
+    Attributes,
+    Files,
+  },
+
+  props: {
+    productGroups: {
+      type: Array,
+      default: () => ([]),
+    },
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
--- gui/src/pages/show-product/index.vue
+++ gui/src/pages/show-product/index.vue
@@ -1,77 +1,55 @@
 <template>
-  <q-page-container v-if="product" class="full-height window-height bg-grey-3">
-    <q-scroll-area class="fit border-right">
+  <q-page-container v-if="product" id="JGHRGHVT" class="full-height window-height bg-grey-4">
+    <q-scroll-area class="fit border-right q-my-md">
       <div class="preview-page bg-white">
         <div class="q-pa-lg">
-          <div class="title text-grey-8">{{ product.manufacturer.name }} {{ product.name }}</div>
+          <div class="row">
+            <div class="col-8">
+              <div class="title text-grey-8">{{ product.manufacturer.name }} {{ product.name }}</div>
+            </div>
+
+            <div class="col-4 text-right">
+              <q-chip
+                v-for="pg of productGroups"
+                :key="pg.id"
+                color="brown-2"
+                square
+              >
+                {{ pg.name }}
+              </q-chip>
+            </div>
+          </div>
         </div>
 
-        <q-list bordered class="rounded-borders">
-          <q-expansion-item
-            :label="`${product.offers.length} Offers`"
-            :value="true"
-            expand-separator
-            header-class="bg-teal-1"
-            icon="list"
+        <q-tabs
+          v-model="tab"
+          :breakpoint="0"
+          active-class="text-white"
+          align="left"
+          class="bg-brown-6 text-grey-5 q-ma-none"
+          indicator-color="brown-3"
+          no-caps
+        >
+          <q-tab :label="`${product.offers.length} Offers`" name="offers" />
+          <q-tab label="Product Details" name="details" />
+        </q-tabs>
+
+        <div style="height: calc(100vH - 255px); overflow-y: auto">
+          <q-tab-panels
+            v-model="tab"
+            animated
+            transition-next="slide-up"
+            transition-prev="slide-down"
           >
-            <q-card flat>
-              <q-card-section class="offers">
-                <div class="row header text-grey-7">
-                  <div class="col-4">Anbieter</div>
-                  <div class="col-2">Lieferzeit</div>
-                  <div class="col-2">Artikelpreis</div>
-                  <div class="col-2">Versandkosten</div>
-                  <div class="col-2">Gesamtpreis</div>
-                </div>
+            <q-tab-panel name="offers">
+              <offers :product="product" />
+            </q-tab-panel>
 
-                <div
-                  v-for="(offer, key) of product.offers"
-                  :key="key"
-                  class="row offer text-blue-8"
-                >
-                  <div class="col-4 text-grey-8">{{ offer.dealer.name }}</div>
-                  <div class="col-2 right">1 bis 2 Wochen</div>
-                  <div class="col-2 right">{{ offer.itemPrice | number(2) }} €</div>
-                  <div class="col-2 right">{{ offer.shippingPrice| number(2) }} €</div>
-                  <div class="col-2 right">{{ offer.totalPrice | number(2) }} €</div>
-                </div>
-              </q-card-section>
-            </q-card>
-          </q-expansion-item>
-
-          <q-expansion-item
-            :value="true"
-            expand-separator
-            header-class="bg-teal-1"
-            icon="table"
-            label="Product Details"
-            style="margin-top: 1px"
-          >
-            <q-card flat>
-              <q-card-section>
-                <files :files="product.files" />
-
-                <attributes
-                  v-for="productGroup of productGroups"
-                  :key="productGroup.id"
-                  :product="product"
-                  :product-group="productGroup"
-                  class="q-mb-md"
-                />
-
-                <q-field
-                  label="Description"
-                  outlined
-                  stack-label
-                >
-                  <template v-slot:control>
-                    <div v-html="product.description" />
-                  </template>
-                </q-field>
-              </q-card-section>
-            </q-card>
-          </q-expansion-item>
-        </q-list>
+            <q-tab-panel name="details">
+              <product-details :product="product" :product-groups="productGroups" />
+            </q-tab-panel>
+          </q-tab-panels>
+        </div>
       </div>
     </q-scroll-area>
   </q-page-container>
@@ -80,13 +58,13 @@
 <script>
 import { mapActions, mapGetters } from 'vuex'
 
-import Attributes from './attributes'
-import Files from './files'
+import Offers from './offers'
+import ProductDetails from './details'
 
 export default {
   components: {
-    Attributes,
-    Files,
+    Offers,
+    ProductDetails,
   },
 
   computed: {
@@ -113,6 +91,10 @@ export default {
     this.loadById(this.productId)
   },
 
+  data: () => ({
+    tab: 'offers',
+  }),
+
   methods: {
     ...mapActions({
       loadById: 'showProducts/loadById',
@@ -121,39 +103,18 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
-.preview-page
-  max-width: 1200px
-  margin: 0 auto
-  border-left: 1px solid #ddd
-  border-right: 1px solid #ddd
-  padding: 8px
+<style lang="sass">
+#JGHRGHVT
+  .preview-page
+    max-width: 1200px
+    margin: 0 auto
+    border-left: 1px solid #ddd
+    border-right: 1px solid #ddd
 
-.title
-  font-size: 20px
-  font-weight: bold
-
-.offers
-  .row > div
-    border-bottom: 1px dotted #ddd
-
-  .row:first-child
-    border-bottom: 2px solid #ccc
-
-  .header > div
-    font-size: 18px
+  .title
+    font-size: 20px
     font-weight: bold
-    text-align: center
-    padding: 8px
 
-  .offer > div
-    font-size: 16px
-    text-align: left
-    padding: 8px
-
-  .offer > .right
-    text-align: right
-
-  .offer > div:last-child
-    font-weight: bold
+  .q-tab__indicator
+    height: 5px
 </style>
