e97d70aeaffd3cd0932e49a0364c662979c99418
Guido Drahota <guido.drahota@kaulsdorf-it-solutions.de>
2022-09-28 17:20:14 +0200

    path renamed

--- backend/src/endpoints/index.ts
+++ backend/src/endpoints/index.ts
@@ -1,17 +1,17 @@
-import { Router } from "express"
+import { Router } from 'express'
 
-import { registerDealerRoutes } from "./dealer"
-import { registerFileRoutes } from "./files"
-import { registerManufacturerRoutes } from "./manufacturer"
-import { registerProductAttributeGroupOfProductGroupRoutes } from "./product-attribute-groups-of-product-groups"
-import { registerProductAttributeRoutes } from "./product-attributes"
-import { registerProductAttributesOfProductGroupRoutes } from "./product-attributes-of-product-group"
-import { registerProductAttributeValueRoutes } from "./product-attribute-values"
-import { registerProductGroupRoutes } from "./product-group"
-import { registerProductRoutes } from "./product"
-import { registerProductSearchRoutes } from "./product-seach"
-import { registerProductToAttributeValueRoutes } from "./product-to-attribute-values"
-import { registerProductToProductGroupRoutes } from "./product-to-product-group"
+import { registerDealerRoutes } from './dealer'
+import { registerFileRoutes } from './files'
+import { registerManufacturerRoutes } from './manufacturer'
+import { registerProductAttributeGroupOfProductGroupRoutes } from './product-attribute-groups-of-product-groups'
+import { registerProductAttributeRoutes } from './product-attributes'
+import { registerProductAttributesOfProductGroupRoutes } from './product-attributes-of-product-group'
+import { registerProductAttributeValueRoutes } from './product-attribute-values'
+import { registerProductGroupRoutes } from './product-group'
+import { registerProductRoutes } from './product'
+import { registerProductSearchRoutes } from './product-search'
+import { registerProductToAttributeValueRoutes } from './product-to-attribute-values'
+import { registerProductToProductGroupRoutes } from './product-to-product-group'
 
 export const registerRoutes = ( app: Router ) => {
   const routes = Router()
