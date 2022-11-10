b3abb44de28de3d371ac87a44f43b6684922661c
Isaac Annan <succianan195@gmail.com>
2022-11-10 12:37:59 +0000

    testing: count letters

--- backend/src/services/count-letters/index.spec.ts
+++ backend/src/services/count-letters/index.spec.ts
@@ -7,9 +7,49 @@ describe( 'countLetters', () => {
     expect( test ).toEqual( 5 )
   } )
 
-  test( 'should return 5', () => {
+  test( 'should return 12', () => {
     const test = countLetters( 'Isaac Allotey' )
 
     expect( test ).toEqual( 12 )
+  } )
+
+  test( 'should return 12', () => {
+    const test = countLetters( 'Isaac Allotey ' )
+
+    expect( test ).toEqual( 12 )
+  } )
+
+  test( 'Should throw an Error', () => {
+    // @ts-ignore
+    expect( () => countLetters( null ) ).toThrow( 'Cannot perform count on a non-string value' )
+  } )
+
+  test( 'should return 12', () => {
+    // @ts-ignore
+    const test = countLetters( 'Isaac_Allotey_' )
+
+    expect( test ).toEqual( 12 )
+  } )
+
+
+  test( 'should return 2', () => {
+    const test = countLetters( '¢$' )
+
+    expect( test ).toEqual( 2 )
+  } )
+
+  test( 'should throw and Error', () => {
+    // @ts-ignore
+    expect(() => countLetters( true ) ).toThrow('Cannot perform count on a non-string value')
+  } )
+
+  test( 'should throw and Error', () => {
+    // @ts-ignore
+    expect(() => countLetters( false ) ).toThrow('Cannot perform count on a non-string value')
+  } )
+
+  test( 'should throw and Error', () => {
+    // @ts-ignore
+    expect(() => countLetters( {} ) ).toThrow('Cannot perform count on a non-string value')
   } )
 } )
--- backend/src/services/count-letters/index.ts
+++ backend/src/services/count-letters/index.ts
@@ -1,3 +1,10 @@
-export const countLetters = ( textValue: string ): number => {
-  return textValue.length
+export const countLetters = ( textValue: string ): number  => {
+  if(typeof textValue !== 'string'){
+    throw new Error('Cannot perform count on a non-string value')
+  }
+
+  return textValue
+    .replace(/ /g, '')
+    .replace(/_/g, '')
+    .length
 }
